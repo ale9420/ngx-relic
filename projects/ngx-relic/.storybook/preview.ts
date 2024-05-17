4711f762ac6880063566dfee8546d4be23470983
Alejandro Ramirez Garcia <76931084+ale9420@users.noreply.github.com>
2024-05-17 16:39:49 -0500

    se actualiza el script para realizar build de storybook y el archivo de github pages

--- projects/ngx-relic/.storybook/preview.ts
+++ projects/ngx-relic/.storybook/preview.ts
@@ -1,6 +1,6 @@
-import type { Preview } from "@storybook/angular";
-import { setCompodocJson } from "@storybook/addon-docs/angular";
-import docJson from "../documentation.json";
+import type { Preview } from '@storybook/angular';
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from '../documentation.json';
 setCompodocJson(docJson);
 
 const preview: Preview = {
@@ -12,6 +12,8 @@ const preview: Preview = {
       },
     },
   },
+
+  tags: ['autodocs'],
 };
 
 export default preview;
